c1938af20f64c02699652035bae22c4f4601e4f0
Michael Telatynski <7t3chguy@gmail.com>
2015-03-22 08:20:30 +0000

    SQL Fix

--- SQL/iredmail+alias.my.sql
+++ SQL/iredmail+alias.my.sql
@@ -4,14 +4,10 @@ CREATE
     SQL SECURITY DEFINER
 VIEW `global_addressbook` AS
     SELECT
-        SUBSTR((UNIX_TIMESTAMP(`vmail`.`mailbox`.`passwordlastchange`) +
-                UNIX_TIMESTAMP(`vmail`.`mailbox`.`modified`)) -
-                UNIX_TIMESTAMP(`vmail`.`mailbox`.`created`),
-            -(8)) AS `ID`,
+        SUBSTR((UNIX_TIMESTAMP(`vmail`.`mailbox`.`created`) -
+        CHAR_LENGTH(`vmail`.`mailbox`.`password`)), -(8)) AS `ID`,
         `vmail`.`mailbox`.`name` AS `name`,
-        (SELECT
-                GROUP_CONCAT(`vmail`.`alias`.`address`
-                        SEPARATOR ',')
+        (SELECT GROUP_CONCAT(`vmail`.`alias`.`address` SEPARATOR ',')
             FROM
                 `vmail`.`alias`
             WHERE
@@ -19,4 +15,4 @@ VIEW `global_addressbook` AS
                     AND (`vmail`.`alias`.`address` LIKE '%@%'))) AS `email`,
         `vmail`.`mailbox`.`domain` AS `domain`
     FROM
-        `vmail`.`mailbox`
+        `vmail`.`mailbox`;
