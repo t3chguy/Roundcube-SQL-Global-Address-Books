5e98be14522f91d3ddce811e5569598af72b3410
Michael Telatynski <7t3chguy@gmail.com>
2015-08-12 18:10:47 +0100

    Very basic Global book functionality

--- sql_global_addressbooks/sql_global_addressbooks.php
+++ sql_global_addressbooks/sql_global_addressbooks.php
@@ -27,30 +27,12 @@ class sql_global_addressbooks extends rcube_plugin {
 		$rcmail = rcmail::get_instance();
 		$config = $rcmail->config;
 		$domain = $rcmail->user->get_username('domain');
-	    $sources= (array) $config->get('autocomplete_addressbooks', array());
+	    $sources= (array)$config->get('autocomplete_addressbooks', array());
 
-	    if ($this->is_enabled('domain')) { $x[] = 'domain'; }
-	    if ($this->is_enabled('global')) { $x[] = 'global'; }
-	    foreach ($config->get('_sql_supportbook', array()) as $z) {
-	    	$c = array_shift($z);
-	    	if (!in_array($domain, $z, true) && !in_array($c, $sources)) { $sources[] = $c; }
+	    foreach ($config->get('ABX', array()) as $hash => $book) {
+	    	$sources[] = $hash;
 	    }
-	    foreach (array('domain', 'global') as $v) {
-	    	if ($this->is_enabled($v) && !in_array($v, $sources)){ $sources[] = $v; }
-	    }
-
 	    $config->set('autocomplete_addressbooks', $sources);
-	}
-
-	private function is_enabled($book) {
-
-		$rc = rcmail::get_instance();
-
-		if ($rc->config->get('_sql_' . $book . 'book_name', false)) {
-			return $this->wlbl(substr($book, 0, 1) . 'b', $rc->user->get_username('domain'));
-		}
-
-		return false;
 	}
 
 	private function touchbook($id, $name, $groups=false) {
@@ -61,17 +43,6 @@ class sql_global_addressbooks extends rcube_plugin {
 			'readonly'     => true,
 			'autocomplete' => true,
 		);
-	}
-
-	private function wlbl($id, $domain) {
-		$rc = rcmail::get_instance();
-		$cf = $rc->config->get('_sql_' . $id . '_read_allowed', array('*'));
-		$fc = $rc->config->get('_sql_' . $id . '_read_hidden',  array());
-
-		if (in_array($domain, $fc)) { return false; }
-		if ($cf === array('*') || in_array($domain, $cf)) { return true; }
-		return false;
-
 	}
 
 	public static function ac($arr, $id) {
@@ -93,36 +64,31 @@ class sql_global_addressbooks extends rcube_plugin {
 
 	public function address_sources($p) {
 		$rc     = rcmail::get_instance();
-		$dm     = $rc->user->get_username('domain');
-		$xc     = $rc->config;
 
-		if (($gb = $xc->get('_sql_globalbook_name', false)) && $this->wlbl('gb', $dm)) {
-			$p['sources']['global'] = $this->touchbook('global', $gb, $xc->get('_sql_globalbook_gp', true));
-		}
-
-		if (($db = $xc->get('_sql_domainbook_name', false)) && $this->wlbl('db', $dm)) {
-			$p['sources']['domain'] = $this->touchbook('domain', $db);
-		}
-
-		if ($sb = $xc->get('_sql_supportbook_list', array())) {
-			foreach ($sb as $csb) {
-				$csbn = array_shift($csb);
-				if (!in_array($dm, $csb)) { $p['sources'][$csbn]= $this->touchbook($csbn, $csbn);
-				}
-			}
-		}
+		foreach ($rc->config->get('ABX', array()) as $hash => $book) {
+	    	if ($book->valid($rc->user->get_username())) {
+		    	$p['sources'][$book->id] = $this->touchbook(
+            		$hash, $book->name, $book->groups
+				);
+	    	}
+	    }
 
 		return $p;
 	}
 
 	public function get_address_book($p) {
 
-		if ($p['id'] === 'global') {
+		$ABX = rcmail::get_instance()->config->get('ABX', FALSE);
+		if ($ABX && isset($ABX[$p['id']])) {
+			$p['instance'] = $ABX[$p['id']];
+		}
+
+		/*if ($p['id'] === 'global') {
 			$p['instance'] = new sql_global_backend('global');
 			$p['instance']->groups = rcmail::get_instance()->config->get('_sql_globalbook_gp', true);
 		} elseif (in_array($p['id'], self::ac(rcmail::get_instance()->config->get('_sql_supportbook', array()), 0))) {
 			$p['instance'] = new sql_global_backend($p['id']);
-		} elseif ($p['id'] === 'domain') { $p['instance'] = new sql_global_backend('domain'); }
+		} elseif ($p['id'] === 'domain') { $p['instance'] = new sql_global_backend('domain'); }*/
 
 		return $p;
 	}
